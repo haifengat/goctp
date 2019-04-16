e34f3541661d76bc2c346563ddabc6f98d9ba8db
海风 <24918700@qq.com>
2019-04-16 15:19:53 +0800

    行情接口对等封装完成,还需进行易用性封装

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "go_ctp"
+import (
+	"fmt"
+	"go_ctp"
+	"time"
+)
 
 func main() {
-	//go_ctp.Text()
-	go_ctp.GenerateStruct()
+	//go_ctp.GenerateDataType()
+	//go_ctp.GenerateStruct()
+	//go_ctp.GenerateMd()
+	go_ctp.InitQuote()
+	//go_ctp.RegisterSpi(0)
+	addr := "tcp://180.168.146.187:10000"
+	go_ctp.RegisterFront(addr)
+	go_ctp.Init()
+	time.Sleep(time.Duration(3) * time.Second)
+	id := go_ctp.TThostFtdcUserIDType{'0', '0', '7', '1', '0', '5'}
+	pwd := go_ctp.TThostFtdcPasswordType{'1'}
+	br := go_ctp.TThostFtdcBrokerIDType{'9', '9', '9', '9'}
+	req := go_ctp.CThostFtdcReqUserLoginField{
+		UserID:   id,
+		Password: pwd,
+		BrokerID: br,
+	}
+	go_ctp.ReqUserLogin(req, 1)
+	time.Sleep(time.Duration(3) * time.Second)
+	go_ctp.SubscribeMarketData([]string{"cu1905"}, 1)
+	fmt.Scanln()
 }
--- src/go_ctp/generater.go
+++ src/go_ctp/generater.go
@@ -9,11 +9,153 @@ import (
 	"strings"
 )
 
+func GenerateMd() {
+	bsFile, err := ioutil.ReadFile("src/ctp_20180109_x64/ThostFtdcMdApi.h")
+	checkErr(err)
+
+	outFile := "src/go_ctp/quote.go"
+	_, err = os.Stat(outFile)
+	if err == nil || os.IsExist(err) { // 文件存在
+		os.Remove(outFile)
+	}
+	f, err := os.OpenFile(outFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	checkErr(err)
+	defer f.Close()
+	strHead := `package go_ctp
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+type CThostFtdcMdSpi uintptr
+
+var (
+	h        *syscall.DLL
+	api, spi uintptr
+	nRequestID      int
+	%s *syscall.Proc
+)
+
+func InitQuote(){
+	// 创建 log目录
+	_, err := os.Stat("log")
+	if err != nil {
+		os.Mkdir("log", os.ModePerm)
+	}
+
+	h = syscall.MustLoadDLL("ctp_quote.dll")
+	//defer h.Release() // 函数结束后会释放导致后续函数执行失败
+
+	api, _, _ = h.MustFindProc("CreateApi").Call()
+	spi, _, _ = h.MustFindProc("CreateSpi").Call()
+	h.MustFindProc("RegisterSpi").Call(api, spi)
+%s
+%s
+}
+`
+
+	var varFunc string   // 函数在var中的声明 uintptr
+	var funDefine string // 主调函数定义 funcRegisterFront := h.MustFindProc("RegisterFront")
+	var funBody string   // 函数主体
+
+	var cbInit string // 在init函数中进行回调函数定义 h.MustFindProc("SetOnFrontConnected").Call(spi, syscall.NewCallback(OnConnect))
+	var cbBody string // 回调函数主体
+
+	// 汉字处理
+	bsFile, _ = simplifiedchinese.GB18030.NewDecoder().Bytes(bsFile)
+	//fmt.Println(string(bsFile))
+	re := regexp.MustCompile(`\t///(.*)\r\n[^v]*virtual\s*(\w*)\s*(\w*)\(([^\)]*)\)`) // 分解函数定义:注释,返回类型,函数名,参数字段两部分
+	funs := re.FindAllStringSubmatch(string(bsFile), -1)
+	for _, fun := range funs {
+		funComment, _, funName, funFields := fun[1], fun[2], fun[3], fun[4]
+		// 回调函数
+		if strings.Index(funName, "On") == 0 {
+			cbBody += fmt.Sprintf("// %s\nfunc %s(", funComment, funName)
+			funContent := ""
+			if funFields != "" { // 参数可能为空
+				re = regexp.MustCompile(`(\w*)\s*([*]?\w*)[,]?\s*`) //参数分解:类型,名称
+				fields := re.FindAllStringSubmatch(funFields, -1)
+				for _, field := range fields {
+					fieldType, fieldName := field[1], field[2]
+					if strings.Index(fieldName, "*") >= 0 {
+						cbBody += fmt.Sprintf("%s uintptr, ", fieldName[1:])
+						funContent += fmt.Sprintf("\tp%s := (*%s)(unsafe.Pointer(%s))\n", fieldType, fieldType, fieldName[1:]) //pCThostFtdcRspInfoField := (*CThostFtdcRspInfoField)(unsafe.Pointer(info))
+						funContent += fmt.Sprintf("\tfmt.Println(p%s)\n", fieldType)
+					} else {
+						cbBody += fmt.Sprintf("%s %s,", fieldName, fieldType)
+					}
+				}
+			}
+			cbBody = strings.TrimRight(cbBody, ", ") // 去掉尾部,
+			cbBody += fmt.Sprintf(") uintptr {\n%s\treturn 0\n}\n\n", funContent)
+			cbInit += fmt.Sprintf("\th.MustFindProc(\"Set%s\").Call(spi, syscall.NewCallback(%s))\n", funName, funName)
+		} else { // 主调函数
+			funContent, funParams, callParams := "", "", ""
+			// 函数定义 funcRegisterFront := h.MustFindProc("RegisterFront")
+			funDefine += fmt.Sprintf("\tfunc%s = h.MustFindProc(\"%s\")\n", funName, funName)
+			varFunc += fmt.Sprintf("func%s,", funName)
+
+			if funFields != "" { // 参数可能为空
+				re = regexp.MustCompile(`(\w*)\s*([*]?\s*\w*[\[]?[\]]?)[,]?\s*`) //参数分解:类型,名称
+				fields := re.FindAllStringSubmatch(funFields, -1)
+				for _, field := range fields {
+					isPtrVar := strings.Index(field[2], "*") == 0
+					fieldType, fieldName := field[1], strings.TrimRight(strings.TrimLeft(field[2], "*"), "[]")
+
+					// 处理char *ppInstrumentID[] ==> []string
+					if strings.Contains(field[2], "[]") {
+						funParams += fmt.Sprintf("%s []string, nCount int", fieldName)
+						callParams += fmt.Sprintf(", uintptr(unsafe.Pointer(&%s)), uintptr(nCount)", fieldName)
+						break
+					} else if isPtrVar { // 带*的变量
+						// string -> uintptr(unsafe.Pointer()) -> uintptr(unsafe.Pointer(bs))
+						if fieldType == "char" {
+							funParams += fmt.Sprintf("%s string, ", fieldName)
+							funContent += fmt.Sprintf("\n\tbs, _ := syscall.BytePtrFromString(%s)", fieldName)
+							callParams += ", uintptr(unsafe.Pointer(bs))"
+						} else { // struct
+							funParams += fmt.Sprintf("%s %s, ", strings.TrimLeft(fieldName, " "), fieldType)
+							callParams += fmt.Sprintf(", uintptr(unsafe.Pointer(&%s))", fieldName)
+						}
+					} else {
+						if fieldName == "nRequestID" {
+							funContent += "\n\tnRequestID++"
+						}
+						funParams += fmt.Sprintf("%s %s, ", strings.TrimLeft(fieldName, " "), fieldType)
+						callParams += fmt.Sprintf(", uintptr(%s)", fieldName)
+					}
+				}
+			}
+			funParams = strings.TrimRight(funParams, ", ")
+			funBody += fmt.Sprintf(`// %s
+func %s(%s){%s
+	func%s.Call(api%s)
+}
+`, funComment, funName, funParams, funContent, funName, callParams)
+		}
+	}
+	varFunc = strings.TrimRight(varFunc, ", ")
+	_, _ = f.WriteString(fmt.Sprintf(strHead, varFunc, funDefine, cbInit))
+	_, _ = f.WriteString("\n")
+	_, _ = f.WriteString(cbBody)
+	_, _ = f.WriteString(funBody)
+}
+
 func GenerateStruct() {
 	bsFile, err := ioutil.ReadFile("src/ctp_20180109_x64/ThostFtdcUserApiStruct.h")
 	checkErr(err)
 
-	f, err := os.OpenFile("src/go_ctp/struct.go", os.O_RDWR|os.O_CREATE, os.ModePerm)
+	outFile := "src/go_ctp/struct.go"
+	_, err = os.Stat(outFile)
+	if err == nil || os.IsExist(err) { // 文件存在
+		os.Remove(outFile)
+	}
+
+	f, err := os.OpenFile(outFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
+
 	checkErr(err)
 	defer f.Close()
 	_, _ = f.WriteString("package go_ctp\n\n")
@@ -120,7 +262,14 @@ func GenerateDataType() {
 			}
 		}
 	}
-	f, err := os.OpenFile("src/go_ctp/datatype.go", os.O_RDWR|os.O_CREATE, os.ModePerm)
+
+	outFile := "src/go_ctp/datatype.go"
+	_, err = os.Stat(outFile)
+	if err == nil || os.IsExist(err) { // 文件存在
+		os.Remove(outFile)
+	}
+
+	f, err := os.OpenFile(outFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
 	checkErr(err)
 	defer f.Close()
 	_, _ = f.WriteString("package go_ctp\n\n")
