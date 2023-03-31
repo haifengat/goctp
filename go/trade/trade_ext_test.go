613245a6e688f4db09c01b783367f08b52a1bfe9
haifengat <hubert28@qq.com>
2023-03-31 19:03:40 +0800

    更新: trade_ext test 登录过程用 chan

--- go/trade/trade_ext_test.go
+++ go/trade/trade_ext_test.go
@@ -9,106 +9,165 @@ import (
 
 func TestTradeExt(t *testing.T) {
 	trd := NewTradeExt()
+	eventChan := make(chan string)
+	errorChan := make(chan def.CThostFtdcRspInfoField)
 	trd.OnFrontConnected = func() {
-		trd.ReqAuthenticateField("9999", "008107", "simnow_client_test", "0000000000000000")
+		eventChan <- "OnFrontConnected"
 	}
 	trd.OnRspAuthenticate = func(pRspAuthenticateField *def.CThostFtdcRspAuthenticateField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
 		if pRspInfo.ErrorID != 0 {
-			fmt.Printf("%+v\n", pRspInfo.ErrorID)
+			errorChan <- *pRspInfo
 		} else {
-			fmt.Println("ReqUserLogin")
-			trd.ReqUserLogin("1")
+			eventChan <- "OnRspAuthenticate"
 		}
 	}
 	trd.OnRspUserLogin = func(pRspUserLogin *def.CThostFtdcRspUserLoginField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
 		if pRspInfo.ErrorID != 0 {
-			fmt.Printf("%+v\n", pRspInfo.ErrorID)
+			errorChan <- *pRspInfo
 		} else {
 			fmt.Printf("%+v\n", pRspUserLogin)
 			fmt.Printf("%+v\n", pRspInfo)
-			trd.ReqSettlementInfoConfirm()
+			eventChan <- "OnRspUserLogin"
 		}
 	}
 	trd.OnRspSettlementInfoConfirm = func(pSettlementInfoConfirm *def.CThostFtdcSettlementInfoConfirmField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
 		if pRspInfo.ErrorID != 0 {
-			fmt.Printf("%+v\n", pRspInfo.ErrorID)
+			errorChan <- *pRspInfo
 		} else {
-			fmt.Println("ReqQryInvestor")
-			trd.ReqQryInvestor()
+			eventChan <- "OnRspSettlementInfoConfirm"
 		}
 	}
 	trd.OnRspQryInvestor = func(pInvestor *def.CThostFtdcInvestorField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("%+v\n", pInvestor)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pInvestor != nil {
+			fmt.Printf("OnRspQryInvestor %+v\n", pInvestor)
+		}
 		if bIsLast {
-			time.Sleep(time.Millisecond * 1100)
-			fmt.Println("ReqQryClassifiedInstrument")
-			trd.ReqQryClassifiedInstrument()
+			eventChan <- "OnRspQryInvestor"
 		}
 	}
 	trd.OnRspQryClassifiedInstrument = func(pInstrument *def.CThostFtdcInstrumentField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("%+v\n", pInstrument)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pInstrument != nil {
+			fmt.Printf("OnRspQryClassifiedInstrument %+v\n", pInstrument)
+		}
 		if bIsLast {
-			time.Sleep(time.Millisecond * 1100)
-			fmt.Println("ReqQryOrder")
-			trd.ReqQryOrder()
+			eventChan <- "OnRspQryClassifiedInstrument"
 		}
 	}
 	trd.OnRspQryOrder = func(pOrder *def.CThostFtdcOrderField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		if pOrder != nil {
-			fmt.Printf("%+v\n", pOrder)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pOrder != nil {
+			fmt.Printf("OnRspQryOrder %+v\n", pOrder)
 		}
 		if bIsLast {
-			time.Sleep(time.Millisecond * 1100)
-			fmt.Println("ReqQryTrade")
-			trd.ReqQryTrade()
+			eventChan <- "OnRspQryOrder"
 		}
 	}
 	trd.OnRspQryTrade = func(pTrade *def.CThostFtdcTradeField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("%+v\n", pTrade)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pTrade != nil {
+			fmt.Printf("OnRspQryTrade %+v\n", pTrade)
+		}
 		if bIsLast {
-			time.Sleep(time.Millisecond * 1100)
-			fmt.Println("ReqQryPosition")
-			trd.ReqQryPosition()
+			eventChan <- "OnRspQryTrade"
 		}
 	}
 	trd.OnRspQryInvestorPosition = func(pInvestorPosition *def.CThostFtdcInvestorPositionField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("%+v\n", pInvestorPosition)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pInvestorPosition != nil {
+			fmt.Printf("OnRspQryInvestorPosition %+v\n", pInvestorPosition)
+		}
 		if bIsLast {
-			fmt.Println("onrsp qry position")
-			time.Sleep(time.Millisecond * 1100)
-			fmt.Println("ReqQryPositionDetail")
-			trd.ReqQryPositionDetail()
+			eventChan <- "OnRspQryInvestorPosition"
 		}
 	}
 	trd.OnRspQryInvestorPositionDetail = func(pInvestorPositionDetail *def.CThostFtdcInvestorPositionDetailField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("%+v\n", pInvestorPositionDetail)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pInvestorPositionDetail != nil {
+			fmt.Printf("OnRspQryInvestorPositionDetail %+v\n", pInvestorPositionDetail)
+		}
 		if bIsLast {
-			fmt.Println("ReqQryPositionDetail")
-			time.Sleep(time.Millisecond * 1100)
-			fmt.Println("ReqQryAccount")
-			trd.ReqQryAccount()
+			eventChan <- "OnRspQryInvestorPositionDetail"
 		}
 	}
 	trd.OnRspQryTradingAccount = func(pTradingAccount *def.CThostFtdcTradingAccountField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("%+v\n", pTradingAccount)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pTradingAccount != nil {
+			fmt.Printf("OnRspQryTradingAccount %+v\n", pTradingAccount)
+		}
 		if bIsLast {
-			fmt.Println("OnRspQryTradingAccount")
-			time.Sleep(time.Millisecond * 1100)
-			trd.ReqOrderInsert("rb2305", "SHFE", def.THOST_FTDC_D_Buy, def.THOST_FTDC_OF_Open, 2300, 3, trd.UserID)
+			eventChan <- "OnRspQryTradingAccount"
 		}
 	}
 	trd.OnRspOrderInsert = func(pInputOrder *def.CThostFtdcInputOrderField, pRspInfo *def.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		fmt.Printf("OnRspOrderInsert %+v\n", pRspInfo)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else if pInputOrder != nil {
+			fmt.Printf("OnRspQryTradingAccount %+v\n", pInputOrder)
+		}
+		if bIsLast {
+			eventChan <- "OnRspOrderInsert"
+		}
 	}
 	trd.OnErrRtnOrderInsert = func(pInputOrder *def.CThostFtdcInputOrderField, pRspInfo *def.CThostFtdcRspInfoField) {
-		fmt.Printf("OnErrRtnOrderInsert %+v\n", pRspInfo)
+		if pRspInfo != nil && pRspInfo.ErrorID != 0 {
+			errorChan <- *pRspInfo
+		} else {
+			fmt.Printf("OnErrRtnOrderInsert %+v\n", pRspInfo)
+		}
 	}
 	trd.OnRtnOrder = func(pOrder *def.CThostFtdcOrderField) {
-		fmt.Printf("%+v\n", pOrder)
+		// fmt.Printf("OnRtnOrder %+v\n", pOrder)
 		fmt.Println(pOrder.RequestID, "::", pOrder.StatusMsg)
 	}
 	trd.Subscribe(def.THOST_TERT_QUICK, def.THOST_TERT_RESTART)
 	trd.ReqConnect("tcp://180.168.146.187:10130")
 
-	select {}
+	for {
+		select {
+		case cb := <-eventChan:
+			switch cb {
+			case "OnFrontConnected": // 连接
+				trd.ReqAuthenticateField("9999", "008107", "simnow_client_test", "0000000000000000")
+			case "OnRspAuthenticate": // 认证
+				trd.ReqUserLogin("1")
+			case "OnRspUserLogin": // 登录
+				trd.ReqSettlementInfoConfirm()
+			case "OnRspSettlementInfoConfirm": // 确认结算
+				trd.ReqQryInvestor()
+			case "OnRspQryInvestor": // 查用户
+				time.Sleep(time.Millisecond * 1100)
+				trd.ReqQryClassifiedInstrument()
+			case "OnRspQryClassifiedInstrument": // 查合约
+				time.Sleep(time.Millisecond * 1100)
+				trd.ReqQryOrder()
+			case "OnRspQryOrder": // 查委托
+				time.Sleep(time.Millisecond * 1100)
+				trd.ReqQryTrade()
+			case "OnRspQryTrade": // 查成交
+				time.Sleep(time.Millisecond * 1100)
+				trd.ReqQryPosition()
+			case "OnRspQryInvestorPosition": //查持仓
+				time.Sleep(time.Millisecond * 1100)
+				trd.ReqQryPositionDetail()
+			case "OnRspQryInvestorPositionDetail": // 查持仓明细
+				time.Sleep(time.Millisecond * 1100)
+				trd.ReqQryAccount()
+			case "OnRspQryTradingAccount": // 查持仓后,发送委托
+				fmt.Println("登录过程完成")
+				// trd.ReqOrderInsert("rb2305", "SHFE", def.THOST_FTDC_D_Buy, def.THOST_FTDC_OF_Open, 2300, 3, trd.UserID)
+			default:
+			}
+		case rspInfo := <-errorChan:
+			fmt.Printf("%+v\n", rspInfo)
+		}
+	}
 }
