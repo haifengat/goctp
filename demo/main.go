5e50c3f7799a87530e8a769c015b75bf62b12de1
海风 <hubert28@qq.com>
2024-02-22 15:30:12 +0800

    更新: demo 用 flag 优化

--- demo/main.go
+++ demo/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -10,21 +10,37 @@ import (
 )
 
 func main() {
+	var trdFront, broker, user, pwd, appid, code, inst string
+	var price float64
+	var lot, period, times int
+
+	flag.StringVar(&trdFront, "trdFront", "180.168.146.187:10202", "交易前置")
+	flag.StringVar(&broker, "broker", "9999", "broker")
+	flag.StringVar(&user, "user", "008107", "帐号")
+	flag.StringVar(&pwd, "pwd", "1", "密码")
+	flag.StringVar(&appid, "appID", "simnow_client_test", "appID")
+	flag.StringVar(&code, "code", "0000000000000000", "authCode")
+	flag.StringVar(&inst, "inst", "rb2410", "合约")
+	flag.Float64Var(&price, "price", 0.0, "价格")
+	flag.IntVar(&lot, "lot", 1, "手数")
+	flag.IntVar(&period, "period", 40, "间隔(ms)")
+	flag.IntVar(&times, "times", 1, "发单次数")
+	flag.Parse()
+
 	// 测试多帐号
-	go trdTest("008105", "1")
-	time.Sleep(time.Second * 20)
-	go trdTest("008107", "1")
+	go trdTest(trdFront, broker, user, pwd, appid, code, inst, price, lot, period, times)
+	// time.Sleep(time.Second * 20)
+	// go trdTest("008107", "1")
 	select {}
 }
 
-func trdTest(user, pwd string) {
+func trdTest(trdFront, broker, user, pwd, appid, code, instrument string, price float64, lot, period, times int) {
 	trd := goctp.NewTradePro()
-	var lastPrice float64
 	trd.OnOrder = func(pOrder *goctp.CThostFtdcOrderField) {
-		fmt.Printf("%s,OnOrder\n", trd.InvestorID)
+		fmt.Printf("%s,OnOrder: %+v\n", trd.InvestorID, pOrder)
 	}
 	trd.OnTrade = func(pTrade *goctp.CThostFtdcTradeField) {
-		fmt.Printf("%s,OnTrade\n", trd.InvestorID)
+		fmt.Printf("%s,OnTrade: %+v\n", trd.InvestorID, pTrade)
 	}
 	trd.OnRtnInstrumentStatus = func(pInstrumentStatus *goctp.CThostFtdcInstrumentStatusField) {}
 
@@ -35,8 +51,8 @@ func trdTest(user, pwd string) {
 		fmt.Printf("OnErrRtnOrderAction %+v\n", pRspInfo)
 	}
 	trd.OnRspQryDepthMarketData = func(pDepthMarketData *goctp.CThostFtdcDepthMarketDataField, pRspInfo *goctp.CThostFtdcRspInfoField, nRequestID int, bIsLast bool) {
-		if pDepthMarketData != nil && string(bytes.TrimRight(pDepthMarketData.InstrumentID[:], "\x00")) == "rb2310" {
-			lastPrice = float64(pDepthMarketData.AskPrice1)
+		if pDepthMarketData != nil && pDepthMarketData.InstrumentID.String() == "rb2310" {
+			price = float64(pDepthMarketData.AskPrice1)
 		}
 		// fmt.Println(pDepthMarketData.InstrumentID, "::", lastPrice)
 	}
@@ -59,8 +75,8 @@ func trdTest(user, pwd string) {
 		} else if pTransferBank != nil {
 			fmt.Printf("%+v\n", pTransferBank)
 		}
-		if bIsLast {
-		}
+		// if bIsLast {
+		// }
 	}
 	trd.OnErrRtnBankToFutureByFuture = func(pReqTransfer *goctp.CThostFtdcReqTransferField, pRspInfo *goctp.CThostFtdcRspInfoField) {
 		fmt.Printf("OnErrRtnBankToFutureByFuture %+v\n", pRspInfo)
@@ -73,12 +89,12 @@ func trdTest(user, pwd string) {
 	}
 
 	info, rsp := trd.Start(goctp.LoginConfig{
-		Front:    "tcp://180.168.146.187:10130",
-		Broker:   "9999",
+		Front:    "tcp://" + trdFront,
+		Broker:   broker,
 		UserID:   user,
 		Password: pwd,
-		AppID:    "",
-		AuthCode: "",
+		AppID:    appid,
+		AuthCode: code,
 	})
 	if rsp.ErrorID != 0 {
 		fmt.Printf("%+v\n", rsp)
@@ -86,23 +102,20 @@ func trdTest(user, pwd string) {
 	}
 	fmt.Printf("%+v\n", info)
 
-	// testAction
-	trd.ReqQryDepthMarketData("SHFE", "rb2310")
-	time.Sleep(1 * time.Second)
-	if lastPrice == 0 {
-		fmt.Println("行情未订阅")
-		return
+	if len(instrument) > 0 {
+		for i := 0; i < times; i++ {
+			_, rsp := trd.ReqOrderInsertLimit(instrument, goctp.THOST_FTDC_D_Sell, goctp.THOST_FTDC_OF_Open, price, lot)
+			if rsp.ErrorID != 0 {
+				fmt.Printf("%+v\n", rsp)
+				return
+			}
+			time.Sleep(time.Millisecond * time.Duration(period))
+		}
 	}
-	id, rsp := trd.ReqOrderInsertLimit("rb2310", goctp.THOST_FTDC_D_Sell, goctp.THOST_FTDC_OF_Open, lastPrice+20, 3)
-	if rsp.ErrorID != 0 {
-		fmt.Printf("%+v\n", rsp)
-		return
-	}
-	time.Sleep(1 * time.Second)
-	trd.ReqOrderAction(id)
+	// trd.ReqOrderAction(id)
 	//testIn
 	// trd.ReqQryAccountregister()
 	// time.Sleep(1 * time.Second)
 	// trd.ReqFromBankToFutureByFuture(regInfo.BankAccount.String(), "bankPwd", "", 100)
-	select {}
+	// select {}
 }
